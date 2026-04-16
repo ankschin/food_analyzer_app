78dfc36ad657c7e3987d1baae1193aa4c85385be
Sachin Kumar <sachukr76@gmail.com>
2026-04-16 13:35:03 +0530

    fix: add NativeWind cache dir to   Metro watchFolders for CI

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,10 @@ const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
+// Ensure Metro scans the NativeWind cache directory at startup
+const nativewindCacheDir = path.resolve(__dirname, "node_modules/.cache/nativewind");
+config.watchFolders = [...(config.watchFolders || []), nativewindCacheDir];
+
 module.exports = withNativeWind(config, {
   input: path.resolve(__dirname, "global.css").replace(/\\/g, "/"),
 });
